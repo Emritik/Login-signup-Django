2a2734ab676a5cf69a2b224ebdb90a6d3f3fdabe
Emritik <sharmar152004@gmail.com>
2023-12-17 20:36:19 +0530

    all commits

--- Login signup using django/home/views.py
+++ Login signup using django/home/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import logout, authenticate, login
- 
 
 # Create your views here.
 
